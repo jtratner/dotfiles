a3ffb426ba64a495014e858c1f1c398b7d03766e
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2012-05-15 22:34:30 -0400

    made media-aliases actually work

--- default-programs/media-aliases.zsh
+++ default-programs/media-aliases.zsh
@@ -2,18 +2,24 @@
 # just type filename if a supported filetype
 # TODO: write this as per-system, so can do differently for osx
 #
-alias -s pdf=`where xpdf`
+alias -s pdf=xpdf
 # I set up a separate $DEFAULTEDITORFAULTEDITOR so I can change it on the fly without
 # affecting other editor calls
-export DEFAULTEDITOR=`where vim`
-alias -s zsh=$DEFAULTEDITOR txt=$DEFAULTEDITOR sh=$DEFAULTEDITOR py=$DEFAULTEDITOR m=$DEFAULTEDITOR rst=$DEFAULTEDITOR markdown=$DEFAULTEDITOR md=$DEFAULTEDITOR
-alias -s down=$DEFAULTEDITOR
 
 # let me choose which editor on the fly
-alias usegvim="export DEFAULTEDITOR=`where gvim`"
-alias usevim="export DEFAULTEDITOR=`where vim`"
-alias usegedit="export DEFAULTEDITOR=`where gedit`"
 
+function change-default-editor() {
+export DEFAULTEDITOR=$1
+alias -s zsh=$DEFAULTEDITOR txt=$DEFAULTEDITOR sh=$DEFAULTEDITOR py=$DEFAULTEDITOR m=$DEFAULTEDITOR rst=$DEFAULTEDITOR markdown=$DEFAULTEDITOR md=$DEFAULTEDITOR
+alias -s down=$DEFAULTEDITOR
+}
+
+# set our initial editor as vim
+change-default-editor vim
+
+alias usegvim="change-default-editor gvim"
+alias usevim="change-default-editor vim"
+alias usegedit="change-default-editor gedit"
 # use octave to open .mat files if nothing else selected
-alias -s mat=`where octave`
+alias -s mat=octave
 
