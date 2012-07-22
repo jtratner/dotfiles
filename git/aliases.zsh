4c0a43cd811345a23dcd84bac3b9455a163671e0
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2012-07-21 23:40:33 -0400

    using hub for realz now

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -12,6 +12,7 @@ alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C
 alias gp='git push origin HEAD'
 alias gd='git diff'
 alias gc='git commit'
+alias ga="git add -p"
 alias gca='git commit -a'
 alias gco='git checkout'
 alias gb='git branch'
