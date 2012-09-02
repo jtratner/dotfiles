a28a05359ad52b07bea7f6dea6f41d0f1c87e503
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2012-09-02 19:24:51 -0400

    better django startup

--- django/aliases.zsh
+++ django/aliases.zsh
@@ -5,6 +5,14 @@ alias pmsp="python manage.py startproject"
 alias pmsdb="python manage.py syncdb"
 alias unset-django="unset $DJANGO_SETTINGS_MODULE"
 alias set-django="export DJANGO_SETTINGS_MODULE="
-p-django () {
-    DJANGO_SETTINGS_MODULE=$1 python
+# either you can pass a directory with "settings" in it OR
+# it will just grab the name of the current working directory
+pdjango () {
+    if (( $# == 0))
+    then
+        DJANGO_SETTINGS_MODULE=${PWD##*/}.settings python
+    else
+        DJANGO_SETTINGS_MODULE=$1.settings python
+    fi
 }
+alias pd=pdjango
