fc5f2233ff2807cadc91be25a58f806bde241dd0
Jeffrey Tratner <jeffrey.tratner@gmail.com>
2013-08-12 03:05:57 -0400

    better git completions and aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -18,6 +18,7 @@ alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
 alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+alias grc="git rebase --continue"
 
 
 # cd to the current git or hg repo root
---
+++ git/completion.sh
@@ -0,0 +1,4 @@
+compdef _git-commit gc
+compdef _git-add ga
+compdef _git-branch gb
+compdef _git-diff gd
