77de71a40f68ef003f43eff996a21a865c0866f8
Jeff Tratner <jeffrey.tratner@gmail.com>
2023-09-23 22:00:46 -0700

    Clean up fzf install

--- homebrew/install_stuff.sh
+++ homebrew/install_stuff.sh
@@ -2,18 +2,16 @@
 set -o nounset
 set -e
 set -x
-brew -h || (echo "Installing homebrew" && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
-function install_if_missing () {
-    brew list $1 &> /dev/null || brew install $1
-}
-# Javascript manipulation - important!
-install_if_missing jq
-install_if_missing fx
-install_if_missing parallel
-install_if_missing gprof2dot
-# ag - etc
-install_if_missing the_silver_searcher
-install_if_missing fzf
-install_if_missing xsv
-/usr/local/opt/fzf/install
+brew -h # || (echo "Installing homebrew" && /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)")
+brew install jq `# pretty-print javascript` \
+fx \
+parallel \
+gprof2dot \
+the_silver_searcher `# ag` \
+fzf `# fuzzy search` \
+xsv \
+neovim \
+pyenv `# python environment installer` \
+iterm2
+$(brew --prefix)/opt/fzf/install
 
